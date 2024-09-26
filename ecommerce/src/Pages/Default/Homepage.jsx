f5200b71c76cad6140964c88b14b9b0743779c3a
Dharmik Vadgama <dharmik.ubs@gmail.com>
2024-09-26 16:52:10 +0530

    update

--- ecommerce/src/Pages/Default/Homepage.jsx
+++ ecommerce/src/Pages/Default/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { ReactLenis, useLenis } from 'lenis/react'
 import {
     PRODUCT_ONE,
     PRODUCT_TWO,
@@ -15,6 +16,9 @@ import Card from '../../Components/Common/Card'
 
 
 const Homepage = () => {
+    const lenis = useLenis(({ scroll }) => {
+        // called every scroll
+    })
     const productData = [
         {
             id: 1,
@@ -75,32 +79,34 @@ const Homepage = () => {
     ]
     return (
         <>
-            <section className='lg:py-[7.5rem] py-[4rem] font-quicksand'>
-                <div className='flex justify-between items-center mb-10'>
-                    <span className='lg:text-4xl text-2xl'>New Arrivals</span>
+            <ReactLenis root>
+                <section className='lg:py-[7.5rem] py-[4rem] font-quicksand'>
+                    <div className='flex justify-between items-center mb-10'>
+                        <span className='lg:text-4xl text-2xl'>New Arrivals</span>
+                        <div>
+                            <Link to={`/shop`}>
+                                <button className='uppercase lg:py-3.5 py-2.5 px-6 border border-pink-500 text-pink-500 font-semibold hover:bg-pink-500 hover:text-white transition-colors duration-300 ease-in-out'>Shop Now</button>
+                            </Link>
+                        </div>
+                    </div>
                     <div>
-                        <Link to={`/shop`}>
-                            <button className='uppercase lg:py-3.5 py-2.5 px-6 border border-pink-500 text-pink-500 font-semibold hover:bg-pink-500 hover:text-white transition-colors duration-300 ease-in-out'>Shop Now</button>
-                        </Link>
+                        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-items-center">
+                            {
+                                productData.map((items) => (
+                                    <div key={items.id}>
+                                        <Card
+                                            imgsrc={items.img}
+                                            product_category={items.category}
+                                            product_name={items.name}
+                                            product_price={items.price.toFixed(2)}
+                                        />
+                                    </div>
+                                ))
+                            }
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-items-center">
-                        {
-                            productData.map((items) => (
-                                <div key={items.id}>
-                                    <Card
-                                        imgsrc={items.img}
-                                        product_category={items.category}
-                                        product_name={items.name}
-                                        product_price={items.price.toFixed(2)}
-                                    />
-                                </div>
-                            ))
-                        }
-                    </div>
-                </div>
-            </section>
+                </section>
+            </ReactLenis>
         </>
     )
 }
