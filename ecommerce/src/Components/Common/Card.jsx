3811a0363255ac6318fae6868eaa433d02d127a3
Dharmik Vadgama <dharmik.ubs@gmail.com>
2024-09-28 18:38:58 +0530

    update

--- ecommerce/src/Components/Common/Card.jsx
+++ ecommerce/src/Components/Common/Card.jsx
@@ -15,13 +15,13 @@ const Card = (
                 <img src={imgsrc} alt="" className='w-[260px] object-cover mb-1 pointer-events-none' />
                 <span className='text-[#979797] mb-1 text-sm capitalize'>{product_category}</span>
                 <span className='text-black mb-1 text-[16px]'>{product_name}</span>
-                <div className='flex mb-1'>
+                {/* <div className='flex mb-1'>
                     <StarIcon className='w-4 h-4 cursor-pointer text-[#646467]' />
                     <StarIcon className='w-4 h-4 cursor-pointer text-[#646467]' />
                     <StarIcon className='w-4 h-4 cursor-pointer text-[#646467]' />
                     <StarIcon className='w-4 h-4 cursor-pointer text-[#646467]' />
                     <StarIcon className='w-4 h-4 cursor-pointer text-[#646467]' />
-                </div>
+                </div> */}
                 <div>
                     <span className='text-[#979797] font-semibold'> $ {product_price}</span>
                 </div>
