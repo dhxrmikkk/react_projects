40c30d125cd3b54a4147c38bdff7f017c356fb05
Dharmik Vadgama <dharmik.ubs@gmail.com>
2024-10-01 18:37:16 +0530

    update

--- ecommerce/src/Pages/Default/Cart.jsx
+++ ecommerce/src/Pages/Default/Cart.jsx
@@ -13,54 +13,64 @@ const Cart = () => {
         }
     };
 
+    // Calculate the subtotal
+    const subtotal = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
     return (
         <ReactLenis root>
             <main>
                 <section className="px-4 sm:px-8 md:px-16 xl:px-32 2xl:px-64 py-[5rem]">
                     <div className="relative overflow-x-auto font-quicksand">
                         {cart.length > 0 ? (
-                            <table className="w-full text-sm text-left rtl:text-right text-gray-500">
-                                <thead className="border-b border-black/15 uppercase tracking-widest">
-                                    <tr>
-                                        <td scope="col" className="px-6 py-3">Item</td>
-                                        <td scope="col" className="px-6 py-3">Price</td>
-                                        <td scope="col" className="px-6 py-3">Quantity</td>
-                                        <td scope="col" className="px-6 py-3">Total</td>
-                                        <td scope="col" className="px-6 py-3">Remove</td>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {cart.map((item) => (
-                                        <tr key={item.id} className="border-b border-black/10">
-                                            <td className="px-6 py-4 flex items-center min-w-48">
-                                                <img src={item.image} alt={item.name} className="w-20 h-20 object-cover" />
-                                                <span className="ml-4">{item.name}</span>
-                                            </td>
-                                            <td className="px-6 py-4">{`$${item.price.toFixed(2)}`}</td>
-                                            <td className="px-6 py-4">
-                                                <div>
-                                                    <div className='flex items-center space-x-4'>
-                                                        <button className='text-xl font-quicksand' onClick={() => updateQuantity(item.id, item.quantity > 1 ? item.quantity - 1 : 1)}>-</button>
-                                                        <input
-                                                            type="text"
-                                                            className='p-3.5 max-w-14 bg-white border border-[#6666ff29] text-center text-black disabled:text-black'
-                                                            disabled
-                                                            value={item.quantity}
-                                                        />
-                                                        <button className='text-xl font-quicksand' onClick={() => updateQuantity(item.id, item.quantity < 10 ? item.quantity + 1 : 10)}>+</button>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td className="px-6 py-4">{`$${(item.price * item.quantity).toFixed(2)}`}</td>
-                                            <td className="px-6 py-4">
-                                                <button onClick={() => removeFromCartProduct(item.id)}>
-                                                    <TrashIcon className="w-5 h-5 text-red-600 hover:text-red-800" />
-                                                </button>
-                                            </td>
+                            <>
+                                <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+                                    <thead className="border-b border-black/15 uppercase tracking-widest">
+                                        <tr>
+                                            <td scope="col" className="px-6 py-3">Item</td>
+                                            <td scope="col" className="px-6 py-3">Price</td>
+                                            <td scope="col" className="px-6 py-3">Quantity</td>
+                                            <td scope="col" className="px-6 py-3">Total</td>
+                                            <td scope="col" className="px-6 py-3">Remove</td>
                                         </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        {cart.map((item) => (
+                                            <tr key={item.id} className="border-b border-black/10">
+                                                <td className="px-6 py-4 flex items-center min-w-48">
+                                                    <img src={item.image} alt={item.name} className="w-20 h-20 object-cover" />
+                                                    <span className="ml-4">{item.name}</span>
+                                                </td>
+                                                <td className="px-6 py-4">{`$${item.price.toFixed(2)}`}</td>
+                                                <td className="px-6 py-4">
+                                                    <div>
+                                                        <div className='flex items-center space-x-4'>
+                                                            <button className='text-xl font-quicksand' onClick={() => updateQuantity(item.id, item.quantity > 1 ? item.quantity - 1 : 1)}>-</button>
+                                                            <input
+                                                                type="text"
+                                                                className='p-3.5 max-w-14 bg-white border border-[#6666ff29] text-center text-black disabled:text-black'
+                                                                disabled
+                                                                value={item.quantity}
+                                                            />
+                                                            <button className='text-xl font-quicksand' onClick={() => updateQuantity(item.id, item.quantity < 10 ? item.quantity + 1 : 10)}>+</button>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td className="px-6 py-4">{`$${(item.price * item.quantity).toFixed(2)}`}</td>
+                                                <td className="px-6 py-4">
+                                                    <button onClick={() => removeFromCartProduct(item.id)}>
+                                                        <TrashIcon className="w-5 h-5 text-red-600 hover:text-red-800" />
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                                <div className='flex justify-end my-5 text-lg tracking-widest'>
+                                    <div>
+                                        Subtotal : <span>{`$ ${subtotal.toFixed(2)}`}</span>
+                                    </div>
+                                </div>
+                            </>
                         ) : (
                             <h2 className='flex justify-center md:text-lg uppercase items-center h-96 ps-0 md:ps-0 lg:ps-16'>OOPS ! Cart is Empty</h2>
                         )}
